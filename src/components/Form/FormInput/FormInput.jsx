759bfa7178690868c7f3d9eb9771401be6fbfbcf
Tania <Zamotailo.t@gmail.com>
2019-10-21 23:25:23 +0300

    10 lection

--- src/components/Form/FormInput/FormInput.jsx
+++ src/components/Form/FormInput/FormInput.jsx
@@ -1,37 +1,31 @@
 import React, { useContext } from 'react';
 import s from './FormInput.module.scss';
 import { FormContext } from '../FormContainer/FormContainer';
+import FormContainer from '../FormContainer/FormContainer';
 
 function FormInput({label, name, validate, ...props}) {
-  const context = useContext(FormContext);
+  const { formState, onChange, setError, getError } = useContext(FormContext);
+
+  function handleChange(name, value) {
+    if (validate) {
+      setError(name, validate(value));
+    }
+    onChange(name, value);
+  }
+  const error = getError(name);
 
   return (
-    <FormContext>
-      {({ formState, onChange, setError, getError}) => {
-        function handleChange(name, value) {
-          if (validate) {
-            setError(name, validate(value));
-          }
-          onChange(name, value);
-        }
-
-        const error = getError(name);
-
-        return (
-          <div className={s.container}>
-            {/*{error && <div className={s.error}>{error}</div>}*/}
-            {props.children({
-              ...props,
-              error,
-              handleChange,
-              value: formState[name] })}
-          </div>
-        )
-      }}
-    </FormContext>
+    <FormContainer>
+      <div className={s.container}>
+        {/*{error && <div className={s.error}>{error}</div>}*/}
+        {props.children({
+          ...props,
+          error,
+          handleChange,
+          value: formState[name] })}
+      </div>
+    </FormContainer>
   )
 }
-
-
 
 export default FormInput;
